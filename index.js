e762e321226bd4e669ab2aae3020e0eb7feefef4
AkS-ASJ <Sayba11@icloud.com>
2023-02-18 06:14:40 +0100

    ready to deploy finished

--- index.js
+++ index.js
@@ -1,69 +1,65 @@
+
+
 const express = require('express')
 const app = express()
-app.use(express.json())
-const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
 
+
+const Person = require('./models/person')
+
+const morgan = require('morgan')
 app.use(cors())
+app.use(express.json())
 app.use(express.static('build'))
 
+
+
 let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
 ]
 
-app.get('/', morgan('tiny'), (request, response) => {
-    response.send('<h1>Welcome to the phonebook</h1>')
-})
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
     const date = new Date();
-    response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${date}</p>`)
+    Person.find({}).then(persons => {
+        console.log(persons.length, date)
+        response.json(`Phonebook has info for ${persons.length} people ${date}`)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = persons.find(note => note.id === id)
+    Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
+})
 
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
     }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedNum => {
+            response.json(updatedNum)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-const generateId = () => {
-    return Math.floor(Math.random()*(100 - persons.length) + persons.length);
-}
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :body'), (request, response) => {
@@ -76,18 +72,31 @@ app.post('/api/persons', morgan(':method :url :status :res[content-length] - :re
         })
     }
 
-    const person = {
+    const person = new Person ({
         name: body.name,
         number: body.number,
-        id: generateId(),
-    }
+    })
 
-    persons = persons.concat(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 
-    response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
